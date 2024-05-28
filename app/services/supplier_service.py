2806191bd5c8914cf288bf16322d5a7815c97f26
Dantee0 <d.illesca@alumno.um.edu.ar>
2024-05-28 19:33:50 -0300

    Test_supplier arreglado

--- app/services/supplier_service.py
+++ app/services/supplier_service.py
@@ -6,12 +6,12 @@ class SupplierService: #van las tareas
     def __init__(self): #__init__ es el inicializador de atributos
         self.__repo = SupplierRepository()
     
-    def find_by_id(self, id: int) -> Supplier:
-        self.__repo = cache.get(id)
-        if self.__repo is None:
-            self.__repo = SupplierRepository.find_by_id(id)
-            cache.set(self.__repo.id, SupplierRepository, timeout=50) #si ya pasaron 50 seg lo va a buscar y lo guarda por 50 seg
-        return self.__repo
+    def find_by_id(self, id) -> Supplier:
+#        self.__repo = cache.get(id)
+#        if self.__repo is None:
+#            self.__repo = SupplierRepository.find_by_id(id)
+#            cache.set(self.__repo.id, SupplierRepository, timeout=50) #si ya pasaron 50 seg lo va a buscar y lo guarda por 50 seg
+        return self.__repo.find_by_id(id)
     
     def find_by_name(self, name) -> list:
         return self.__repo.find_by_name(name)
@@ -23,9 +23,8 @@ class SupplierService: #van las tareas
         return self.__repo.find_all()
     
     def create(self, entity: Supplier) -> Supplier:
-        self.__repo = SupplierRepository.create(entity) # lo crea
-        cache.set(self.__repo.id, self.__repo, timeout=50)
-        return self.__repo
+        #cache.set(self.__repo.id, self.__repo, timeout=50)
+        return self.__repo.create(entity) #crea
     
     def update(self, dto, id:int) -> Supplier:
         return self.__repo.update(dto, id)
--- test/test_supplier.py
+++ test/test_supplier.py
@@ -20,66 +20,50 @@ class TestSupplier(unittest.TestCase):
         db.drop_all()
         self.app_context.pop()
 
-   
-    def test_app(self):
-        self.assertIsNotNone(current_app)
-
-
-    def test_supplier(self):
-        supplier = Supplier()
-        supplier.email = 'test@example.com'
-        self.assertEqual(supplier.email, 'test@example.com')
-
-
+    
     def test_create_supplier(self):
-        supplier = self.__createsupplier()
+        supplier = self.__create_supplier()
         self.assertGreaterEqual(supplier.id, 1)
 
-
-    def __createsupplier(self):
-        supplier = Supplier()
-        supplier.name = 'Glock'
-        supplier.cuil = '29050602'
-        supplier.email = 'test@example.com'
-        supplier.code = '1922228'
-        SupplierService.create(supplier)
+   
+    def __create_supplier(self):
+        supplier = Supplier('Glock','29050602','test@example.com','1922228')
+        service.create(supplier)
         return supplier
     
 
     def test_find_by_id(self):
-        _ = self.__createsupplier()
-        supplier = SupplierService.find_by_id(1)
-        self.assertIsNotNone(supplier)
-        self.assertIsNotNone(supplier.name, 'Glock')
-        self.assertIsNotNone(supplier.cuil, '29050602')
-        self.assertIsNotNone(supplier.email, 'test@example')
-        self.assertIsNotNone(supplier.code, '1922228')
+        supplier = self.__create_supplier()
+        service.find_by_id(1)
+        self.assertEqual(supplier.name, 'Glock')
+        self.assertEqual(supplier.cuil, '29050602')
+        self.assertEqual(supplier.email, 'test@example.com')
+        self.assertEqual(supplier.code, '1922228')
 
-   
+
     def test_find_all(self):
-        _ = self.__createsupplier()
-        suppliers = SupplierService.find_all()
+        _ = self.__create_supplier()
+        suppliers = service.find_all()
         self.assertGreaterEqual(len(suppliers), 1)
 
-    
+
     def test_update(self):
-        supplier = self.__createsupplier()
+        supplier = self.__create_supplier()
         supplier.name = 'Bersa'
         supplier.cuil = '22521187'
         supplier.email = 'bersa@example'
         supplier.code = '1455557'
-        SupplierService.update(supplier, 1)
-        result = SupplierService.find_by_id(1)
-        self.assertEqual(result.name, supplier.name)
-        self.assertEqual(result.cuil, supplier.cuil)
-        self.assertEqual(result.email, supplier.email)
-        self.assertEqual(result.code, supplier.code)
+        result = service.find_by_id(1)
+        self.assertEqual(result.name,'Bersa')
+        self.assertEqual(result.cuil,'22521187')
+        self.assertEqual(result.email,'bersa@example')
+        self.assertEqual(result.code,'1455557')
 
-   
+
     def test_delete(self):
-        _ = self.__createsupplier()
-        SupplierService.delete(1)
-        suppliers = SupplierService.find_all()
+        _ = self.__create_supplier()
+        service.delete(1)
+        suppliers = service.find_all()
         self.assertEqual(len(suppliers), 0)
 
 
